eda8e095a1593adb7cc419df7230fc811e4032d9
Ben Y <GitYBenY-a4h1@mailnicks.org>
2017-03-12 19:26:01 -0700

    add 'Reboot' feature

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ quit_ = False
 while not quit_:
     selector = False
     secondarySelector= True
-    aa=ba=ca='  '
+    aa=ba=ca=da='  '
     row = 0
     selected = False
     # main menu selection
     while not selector: 
+        rowFour = False
 
         print(chr(27) + "[2J")
         now = datetime.datetime.now()
@@ -21,9 +22,10 @@ while not quit_:
         print('   -RobCo Trespasser Management System-')
         print('   ====================================')
         print()
-        print('               ',aa,'Easy',aa, '              ') #easy hacking
-        print('              ',ba,'Medium',ba, '              ') # medium level hacking
-        print('               ',ca,'Hard',ca, '              ') # RobCo patented hard-mode hacking
+        print('               ',aa,'Easy',aa)
+        print('              ',ba,'Medium',ba)
+        print('               ',ca,'Hard',ca)
+        print('              ',da,'Reboot',da)
 
         m = getch.getch()
         # 'w' and 's' work "upside down", because w moves up visually and down in row number, and s is vice versa
@@ -33,10 +35,10 @@ while not quit_:
         elif m == 'w':
             row = row - 1
             if row <= 0:
-                row = 3
+                row = 4
         elif m == 's':
             row = row + 1
-            if row >= 4:
+            if row >= 5:
                 row = 1
         elif m == '\n':
             if row != 0:
@@ -44,36 +46,45 @@ while not quit_:
             pass
         #every press but return will move the "cursor"
         if selected != True:
-            aa=ba=ca='  '
+            aa=ba=ca=da='  '
             if row == 1:
                 aa = '▐▌'
             elif row == 2:
                 ba = '▐▌'
             elif row == 3:
                 ca = '▐▌'
-            else:
-                pass
+            elif row == 4:
+                da = '▐▌'
         else:
-            if row == 1:
+            if row == 4:
+                rowFour = True
+                selected = False
+            elif row == 1:
                 difficulty = security.wordGetter('easy')
             elif row == 2:
                 difficulty = security.wordGetter('medium')
             elif row == 3:
                 difficulty = security.wordGetter('hard')
-            difficulty = security.initrand(difficulty) # get a new dictionary of 5 words instead of 13
-            selector = True
-            secondarySelector = False
+
+            if rowFour == False:
+                difficulty = security.initrand(difficulty) # get a new dictionary of 5 words instead of 13
+                selector = True
+                secondarySelector = False
+                print(difficulty[0]+' '+difficulty[1]+' '+difficulty[2]+' '+difficulty[3]+' '+difficulty[4])
+                time.sleep(1)
+            else:
+                print(chr(27) + "[2J")
+                time.sleep(2)
+                security.t()
+                security.startup()
 
     while not secondarySelector: 
 
         print(chr(27) + "[2J")
 
-        print(row, difficulty)
-
         m = getch.getch()
         if m == 'q':
             secondarySelector = True
-            quit_ = True
         elif m == 'w':
             print('up')
         elif m == 's':
