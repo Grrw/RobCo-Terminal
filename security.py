8c8a6d2a00c60ccdb5acda97eae7e41290bd70cd
Ben Yeffet <GitYBenY-a4h1@mailnicks.org>
2017-03-23 18:53:05 -0700

    implement noise playing

--- security.py
+++ security.py
@@ -1,4 +1,23 @@
-import random, json, time, datetime, curses
+import random, json, time, datetime, curses, pygame
+
+def noise(kind):
+    pygame.mixer.init()
+    if kind == 'poweron':
+        song = pygame.mixer.Sound("audio/poweron.ogg")
+        pygame.mixer.Sound.play(song)
+        time.sleep(0.7)
+    elif kind == 'poweroff':
+        song = pygame.mixer.Sound("audio/poweroff.ogg")
+        pygame.mixer.Sound.play(song)
+        time.sleep(0.3)
+    elif kind == "keys":
+        noise = random.randint(1, 9)
+        key = 'audio/' + str(noise) + '.ogg'
+        song = pygame.mixer.Sound(key)
+        pygame.mixer.Sound.play(song)
+    elif kind == 'select':
+        song = pygame.mixer.Sound("audio/select.ogg")
+        pygame.mixer.Sound.play(song)
 
 def t():
     length = random.gauss(0.15,0.08)
@@ -9,17 +28,17 @@ def startup(stdscr):
     length = random.randrange(30021, 68904, 7)
 
     stdscr.clear()
-    screen = stdscr.subwin(25, 100, 0, 0)
+    screen = stdscr.subwin(25, 80, 0, 0)
     screen.box()
     screen.refresh()
 
-    stdscr.addstr(1,31,'**** ROBCO TERMLINK UPLINK SYSTEM ****',curses.color_pair(1))
+    stdscr.addstr(1,21,'**** ROBCO TERMLINK UPLINK SYSTEM ****',curses.color_pair(1))
     stdscr.refresh()
     startupList = [
         'RobCo-OS v7.6.0.3',
         'SYSTEM 64k RAM',
         'NO HOLOTAPE FOUND',
-        '00b160ff 00064b72 0005aa6c 000a36c0 0007dc12 0007d490 00018471 000b16d3',
+        '00b160ff 00064b72 0005aa6c 000a36c0 0007dc12 0007d490',
         'load 7a 73 6e 74 6c',
          str(length)+' BYTES FREE | OK',
         '120 POKE 736, ' + str(length) + ' x$=""LH) 6;T',
@@ -27,7 +46,7 @@ def startup(stdscr):
         'Get: 2 14kbps [472 B]'
         'No UNIVAC tape found. Mounted drive in /mnt/.',
         'Processor: GE Athlon(tm) 16 Processor 10+'
-        'Memory Testing: '+str(length)+' B (Installed:8984 B)',
+        'Memory Testing: '+str(length)+' B (8984 B)',
         'ASN ABN-SLI energy ACPI rev 1011-010',
          str(now)+'',
         'Temperature: '+str(length)+'°Ra',
@@ -66,34 +85,34 @@ def startup(stdscr):
     stdscr.clear()
     screen.box()
     stdscr.refresh()
-    stdscr.addstr(1,29,'ROBCO INDUSTRIES UNIFIED OPERATING SYSTEM',curses.color_pair(1))
+    stdscr.addstr(1,20,'ROBCO INDUSTRIES UNIFIED OPERATING SYSTEM',curses.color_pair(1))
     stdscr.refresh()
     t()
-    stdscr.addstr(2,31,'COPYRIGHT 2075-2077 ROBCO INDUSTRIES',curses.color_pair(1))
-    stdscr.addstr(3,37,str(now),curses.color_pair(1))
+    stdscr.addstr(2,22,'COPYRIGHT 2075-2077 ROBCO INDUSTRIES',curses.color_pair(1))
+    stdscr.addstr(3,28,str(now),curses.color_pair(1))
     stdscr.refresh()
-    stdscr.addstr(4,46,'[',curses.color_pair(1))
-    stdscr.addstr(4,50,']',curses.color_pair(1))
+    stdscr.addstr(4,37,'[',curses.color_pair(1))
+    stdscr.addstr(4,41,']',curses.color_pair(1))
     t()
     time.sleep(0.5)
     t()
-    stdscr.addstr(5,31,'-RobCo Trespasser Management System-',curses.color_pair(1))
+    stdscr.addstr(5,22,'-RobCo Trespasser Management System-',curses.color_pair(1))
     stdscr.refresh()
     t()
-    stdscr.addstr(6,31,'[',curses.color_pair(1))
-    stdscr.addstr(6,67,']',curses.color_pair(1))
-    acc = 32
+    stdscr.addstr(6,22,'[',curses.color_pair(1))
+    stdscr.addstr(6,57,']',curses.color_pair(1))
+    acc = 23
     number = 0
-    for x in range(35):
+    for x in range(34):
         stdscr.addch(6,acc,ord('='),curses.color_pair(1))
         if number < 10:
-            stdscr.addstr(4,47,'0'+str(number)+'%',curses.color_pair(1))
+            stdscr.addstr(4,38,'0'+str(number)+'%',curses.color_pair(1))
         else:
-            stdscr.addstr(4,47,str(number)+'%',curses.color_pair(1))
+            stdscr.addstr(4,38,str(number)+'%',curses.color_pair(1))
         
         stdscr.refresh()
         acc += 1
-        if acc < 66:
+        if acc < 56:
             number += 3
         t()
 
@@ -101,9 +120,10 @@ def startup(stdscr):
     time.sleep(0.8)
 
 def showcontrols(stdscr, onoff):
-    controlsmenu = stdscr.subwin(8, 74, 15, 2)
-    controlslist = stdscr.subwin(9, 22, 14, 76)
+    controlsmenu = stdscr.subwin(8, 74, 15, 3)
+    controlslist = stdscr.subwin(8, 22, 15, 30)
     if onoff == True:
+        controlslist.clear()
         controlsmenu.box()
         controlsmenu.addstr(1,1,'esc  F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12   psc srl psb',curses.color_pair(1))
         controlsmenu.addstr(2,1,' ~   1  2  3  4  5  6  7  8  9  0 -  = bksp   ins hom pup   nlk /  *  -',curses.color_pair(1))
@@ -116,15 +136,13 @@ def showcontrols(stdscr, onoff):
         controlsmenu.addstr(4,40, ' RET ',curses.color_pair(2))
         controlsmenu.addstr(5,51,' ^ ',curses.color_pair(2))
         controlsmenu.addstr(6,48,' <  V  > ',curses.color_pair(2))
-        controlslist.box()
-        controlslist.addstr(1,6,' CONTROLS ',curses.color_pair(2))
-        controlslist.addstr(2,2,'W or ^ ........ up',curses.color_pair(1))
-        controlslist.addstr(3,2,'S or V ...... down',curses.color_pair(1))
-        controlslist.addstr(4,2,'A or < ...... left',curses.color_pair(1))
-        controlslist.addstr(5,2,'D or > ..... right',curses.color_pair(1))
-        controlslist.addstr(6,2,'E or RET .. select',curses.color_pair(1))
-        controlslist.addstr(7,2,'Q ........... quit',curses.color_pair(1))
     else:
         controlsmenu.clear()
-        controlslist.clear()
+        controlslist.box()
+        controlslist.addstr(1,2,'W or ^ ........ up',curses.color_pair(1))
+        controlslist.addstr(2,2,'S or V ...... down',curses.color_pair(1))
+        controlslist.addstr(3,2,'A or < ...... left',curses.color_pair(1))
+        controlslist.addstr(4,2,'D or > ..... right',curses.color_pair(1))
+        controlslist.addstr(5,2,'E or RET .. select',curses.color_pair(1))
+        controlslist.addstr(6,2,'Q ........... quit',curses.color_pair(1))
     return False
