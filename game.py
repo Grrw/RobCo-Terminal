d0f4bef3a6fd9ae432e5b4b787cf0fdb0debd1c2
Ben Yeffet <GitYBenY-a4h1@mailnicks.org>
2017-09-28 08:50:21 -0700

    properly get a list of words (expand) - no longer crashes the game - words.json changed to a different method

--- game.py
+++ game.py
@@ -1,38 +1,53 @@
-import json, random
+import json, random, curses
 
-def game(difficulty):
+def game(stdscr, difficulty):
 
     level = difficulty
 
     def wordGetter():
-        """
-        Gets words from a list, returning them
-        levels are: easy (5 letters), medium (7 letters), hard (9 letters)
-        """
+        """ Gets words from a list, returning them
+        easy (5 letters, 3 words), 
+        medium (7 letters, 5 words),
+        hard (9 letters, 7 words) """
         nonlocal level
         with open('words.json') as words_:
             word_data = json.load(words_)
+        
+        wrdSet = random.randint(0,3)
+
         if level == "easy":
-            return(word_data["Easy"])
+            return(word_data["Easy"][wrdSet])
         elif level == "medium":
-            return(word_data["Medium"])
+            return(word_data["Medium"][wrdSet])
         elif level == "hard":
-            return(word_data["Hard"])
+            return(word_data["Hard"][wrdSet])
 
-    def initrand(user_list):
-        """
-        Takes words from the list, returning 5 random words
-        """
-        startupList = []
-        for x in range(5):
-            popper = random.randrange(0, len(user_list))
-            startupList.append(user_list[popper])
-            user_list.pop(popper)
-        return startupList
+    # def initrand(user_list, diff):
+    #     """ Takes words from the list,
+    #     returning 5 random words """
+    #     retList = []
+    #     if diff == 'easy':
+    #         wrdAmt = 3
+    #     elif diff == 'medium':
+    #         wrdAmt = 5
+    #     elif dff == 'hard':
+    #         wrdAmt = 7
 
+    #     for x in range(wrdAmt):
+    #         popper = random.randrange(0, len(user_list))
+    #         retList.append(user_list[popper])
+    #         user_list.pop(popper)
+    #     return retList
+
+    """ GAMES START HERE """
     diffList = wordGetter()
-    diffList = initrand(diffList)
     
+    ### temporary starts
+    stdscr.addstr(13,10,'                                              ')
+    stdscr.addstr(13,10,str(diffList),curses.color_pair(1))
+    ### temporary ends
+
+
     if level == 'easy':
         pass
     elif level == 'medium':
