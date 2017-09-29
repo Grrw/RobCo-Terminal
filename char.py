7bea10173fc5b6e7d9337bedfc76099e61e59226
Ben Yeffet <GitYBenY-a4h1@mailnicks.org>
2017-09-29 07:54:06 -0700

    Initial commit

---
+++ char.py
@@ -0,0 +1,4 @@
+""" File for char class
+is length one
+chars are made and decided by the templates
+used in charword for each char """
---
+++ charword.py
@@ -0,0 +1,3 @@
+""" File for the word class
+is a certain length (to be used for curses displaying)
+is composed of characters that are of char class """
