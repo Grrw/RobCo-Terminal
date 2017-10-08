db748c6250a68fccb67c3d613e1bdd9205c3e50e
Ben Yeffet <GitYBenY-a4h1@mailnicks.org>
2017-10-08 09:34:15 -0700

    create functions __init__(), ownedby(), __str__(), string() don't ask why I made __str__ and string

--- robchar.py
+++ robchar.py
@@ -3,4 +3,21 @@ is length one
 chars are made and decided by the templates
 used in charword for each char """
 
-class robchar()
+class robchar():
+
+    def __init__(self, char, owner):
+        """ char is the character that is
+        owner is the charword or False """
+        self.char = char
+        self.owner = owner
+
+    def ownedby(self):
+        return self.owner
+
+    def __str__(self):
+        return self.char
+
+    def string(self):
+        return str(self.char)
+
+    __repr__ = __str__
