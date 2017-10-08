152f141de1307c7c5384d07d45e86872416f938e
Ben Yeffet <GitYBenY-a4h1@mailnicks.org>
2017-10-08 09:34:29 -0700

    create __init__ and rowturn

--- charword.py
+++ charword.py
@@ -1,3 +1,14 @@
 """ File for the word class
 is a certain length (to be used for curses displaying)
 is composed of characters that are of char class """
+
+class charword():
+
+    def __init__(self, row, length):
+        """
+        """
+        self.row = row
+        self.length = length
+
+    def rowturn(self):
+        return self.row
